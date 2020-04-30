20c9e479814f7dd8c3094866daca72caee312465
Lastiqe <62081402+Lastiqe@users.noreply.github.com>
2020-04-30 15:49:38 +0300

    create structure of project, checkpoint

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
+    <div >
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
